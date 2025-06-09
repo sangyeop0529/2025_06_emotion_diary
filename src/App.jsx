aface775cb33a004d31d7fa6c48e13bb25645d32
DELL <sangyeop0529@gmail.com>
2025-06-09 23:02:00 +0900

    image 모듈화

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 function App() {
-  const nav = useNavigate();
-  const handleClick = () => nav("/new");
   return (
     <>
       <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary/123"}>Diary</Link>
+        <img src={getEmotionImage(1)} alt="완전 기쁨" />
+        <img src={getEmotionImage(2)} alt="기쁨" />
+        <img src={getEmotionImage(3)} alt="보통" />
+        <img src={getEmotionImage(4)} alt="나쁨" />
+        <img src={getEmotionImage(5)} alt="완전 나쁨" />
       </div>
-      <button onClick={handleClick}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
