dca0b0f275995f832a24d6c7ee574ed44feae2eb
DELL <sangyeop0529@gmail.com>
2025-06-09 23:23:00 +0900

    Header componet 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,10 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <Header />
-      <Button
-        text="보통"
-        onClick={() => {
-          console.log("normal");
-        }}
-      />
-      <Button
-        text="긍정"
-        onClick={() => {
-          console.log("POSITIVE");
-        }}
-        type={"POSITIVE"}
-      />
-      <Button
-        text="부정"
-        onClick={() => {
-          console.log("NEGATIVE");
-        }}
-        type={"NEGATIVE"}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
