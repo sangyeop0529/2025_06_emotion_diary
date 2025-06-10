98e80bae4d67c78c52b30f14886ceff4e4ff8adf
DELL <sangyeop0529@gmail.com>
2025-06-10 13:14:27 +0900

    diary 페이지 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,30 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "./Editor.css";
 import Button from "./Button";
 import EmotionItem from "./EmotionItem";
-
-const emotionList = [
-  { emotionId: 1, emotionName: "완전 좋음" },
-  { emotionId: 2, emotionName: "좋음" },
-  { emotionId: 3, emotionName: "그럭저럭" },
-  { emotionId: 4, emotionName: "나쁨" },
-  { emotionId: 5, emotionName: "끔찍함" },
-];
-
-const getStringedDate = (targetDate) => {
-  // YYYY-MM-DD
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (date < 10) {
-    date = `0${date}`;
-  }
-
-  return `${year}-${month}-${date}`;
-};
+import { emotionList } from "../utils/constants";
+import { getStringedDate } from "../utils/get-stringed-date";
 
 const Editor = ({ onSubmit, initData }) => {
   const [input, setInput] = useState({
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../utils/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
+  const nav = useNavigate();
 
-  return <div>Diary {params.id}번</div>;
+  const curDiaryItem = useDiary(params.id);
+
+  if (curDiaryItem === undefined) {
+    return <div>데이터 로딩중...!</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
